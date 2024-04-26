c8584d49aa6acd567daa6e5dce7045cafa945381
chris <chrisonyeeofficial@gmail.com>
2024-04-26 04:53:27 +0100

    commit to change workstation2

---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+vpc_cidr_block = "10.0.0.0/16"
+my_public_subnets = "10.0.0.1/24"
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "vpc_cidr_block" {
+  description = "cidr_block for my vpc"
+  type = string
+}
+
+variable "my_public_subnets" {
+  description = "my public subnets"
+  type = string
+}
